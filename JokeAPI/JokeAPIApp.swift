fbcee0a656f91c62653c69ecd20bd75a884f076d
William Eze <eze.w@etoncollege.org.uk>
2024-06-04 13:35:20 +0100

    Initial Commit

---
+++ JokeAPI/JokeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeAPIApp.swift
+//  JokeAPI
+//
+//  Created by William Eze on 04/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct JokeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
